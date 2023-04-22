b55343ae811f98295df36d67798667715c0102ca
Vishnu <yadav143Vishnu@gmail.com>
2023-04-23 04:16:31 +0530

    error in routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './About/about/about.component';
+import { HomeComponent } from './Home/home/home.component';
+
+const routes: Routes = [
+  
+    {
+      path:'', component:HomeComponent
+    },
+    {
+      path:'/about', component:AboutComponent
+    }
+ 
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
